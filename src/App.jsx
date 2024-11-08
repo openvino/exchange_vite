296a312f8fa37df04c048289960eb4fe0498ea00
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2024-11-08 14:44:55 -0300

    fix price fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import './App.css'
-import WinerySelector from './components/WinerySelector/WinerySelector'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Selector from './components/Selector/Selector'
-import Main from './components/Main'
+import React, { useState } from "react";
+import "./App.css";
+import WinerySelector from "./components/WinerySelector/WinerySelector";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Selector from "./components/Selector/Selector";
+import Main from "./components/Main";
 const App = () => {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<WinerySelector />} />
-          <Route path='/:wineryId' element={<Selector />} />
-          <Route path='/:wineryId/:productId' element={<Main />} />
-        </Routes>
-      </Router>
-    </>
-  )
-}
+	const [key, setKey] = useState(0);
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path="/" element={<WinerySelector />} />
+					<Route path="/:wineryId" element={<Selector />} />
+					<Route
+						path="/:wineryId/:productId"
+						element={<Main setKey={setKey} key={key} />}
+					/>
+				</Routes>
+			</Router>
+		</>
+	);
+};
 
-export default App
+export default App;
