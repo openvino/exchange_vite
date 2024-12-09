55f32296fc5dfb1f395e636ffd5e0e34306af25e
Juan Arguello <arguellojuan08@gmail.com>
2024-12-08 23:24:03 -0300

    bridge function

--- src/components/bridge/bridge.jsx
+++ src/components/bridge/bridge.jsx
@@ -1,98 +1,144 @@
 import React, { useEffect, useState } from "react";
-
-import { useParams } from "react-router-dom";
-import { axiosClient } from "../../config/axiosClient";
 import Header from ".././Header/Header";
 import styled from "styled-components";
 import Button from "../shared/Button";
-import { ConnectButton } from "thirdweb/react";
+import { ConnectButton, useActiveAccount } from "thirdweb/react";
 import { client } from "../../config/thirdwebClient";
-import { ethereum } from "thirdweb/chains";
-export default function Bridge({ key, setKey }) {
-    const { wineryId, productId } = useParams();
-    const [tokens, setTokens] = useState([]);
-    const [selectedToken, setSelectedToken] = useState("");
+import { baseSepolia } from "thirdweb/chains";
+import ERC20ABI from '../../contracts/erc20.json';
+import { ethers5Adapter } from "thirdweb/adapters/ethers5";
+import { getContract } from "../../utils";
+import { ethers } from "ethers";
+export default function Bridge() {
+  const [walletAddress, setWalletAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [telegram, setTelegram] = useState("");
+  const [error, setError] = useState("");
+  const [balances, setBalances] = useState([]);
+  const [showBalances, setShowBalances] = useState(false);
 
-    const getProductList = async () => {
-        try {
-            const { data } = await axiosClient.get("/token", {
-                params: { winerie_id: wineryId },
-            });
-            console.log(data);
+  const tokensMatch = [
+    { id: "TB24", address: "0xA6a8baf59C58962a0A1a82e8fF8dcfE3f7aAcF9A" },
+    { id: "MTB18", address: "0xc0CcF602398f03a673C8723065Ff2D5DEC1cE92e" },
+    { id: "MTB19", address: "0x5BA286e5399DBF6626A32334E3c5B8697ca15548" },
+  ];
 
-            setTokens(data); // Asegúrate de que la respuesta tenga esta estructura
-        } catch (error) {
-            console.error("Error fetching tokens:", error);
-        }
-    };
+  const account = useActiveAccount();
 
-    useEffect(() => {
-        getProductList();
-    }, [productId, wineryId]);
+  const library = ethers5Adapter.provider.toEthers({
+    client,
+    chain: baseSepolia,
+  });
 
+  const fetchBalances = async () => {
+    try {
+      const balances = await Promise.all(
+        tokensMatch.map(async (token) => {
+          const contract = getContract(token.address, ERC20ABI, library);
+          const balance = await contract.balanceOf(account.address);
+          const decimals = await contract.decimals();
+          return {
+            id: token.id,
+            address: token.address,
+            balance: balance / 10 ** decimals,
+          };
+        })
+      );
 
+      const validBalances = balances.filter((token) => token.balance > 0);
 
+      if (validBalances.length === 0) {
+        setError("No tienes tokens con balance suficiente para migrar");
+        setTimeout(() => setError(false), 5000);
+        return;
+      }
 
-    const handleTokenSelect = (tokenId) => {
-        setSelectedToken(tokenId);
-    };
+      setBalances(validBalances);
+      setShowBalances(true);
+    } catch (err) {
+      console.error("Error al consultar balances:", err);
+      setError("Hubo un problema al consultar los balances");
+      setTimeout(() => setError(false), 5000);
+    }
+  };
 
-    return (
-        <Header>
-            <Container>
-                <CardWrapper>
-                    <MarketData>
-                        <Title>
-                            Migra tus tokens de mainnet a Base
-                        </Title>
-                        <SubTitle>
-                            Completa el formulario
-                        </SubTitle>
-                        <ConnectButton client={client} chain={ethereum} />
-                        <form>
-                            <FormInput placeholder="Dirección de wallet" type="text" />
-                            <FormInput placeholder="Email" type="text" />
-                            <FormInput placeholder="Telegram" type="text" />
+  const handleMigration = async () => {
+    try {
+      const signer = await ethers5Adapter.signer.toEthers({
+        client: client,
+        chain: baseSepolia,
+        account: account,
+      });
 
-                            {/* Lista para los tokens */}
-                            <TokenListWrapper>
-                                <p style={{ textAlign: "center" }}>Selecciona un token</p>
-                                <TokenList>
-                                    {tokens.map((token) => (
-                                        <TokenItem
-                                            key={token.id}
-                                            onClick={() => handleTokenSelect(token.id)}
-                                            selected={token.id === selectedToken}
-                                        >
-                                            <TokenImage src={token.token_icon} alt={token.name} />
-                                            <TokenInfo>
-                                                <span>{token.name}</span>
-                                                <span>{token.amount}</span>
-                                            </TokenInfo>
-                                        </TokenItem>
-                                    ))}
-                                </TokenList>
-                            </TokenListWrapper>
-                            {selectedToken && <AvailableTokens>
-                                <p> Disponible: 30 tokens {selectedToken}</p>
-                            </AvailableTokens>}
-                            <ButtonFrame text="Migrar" />
-                        </form>
-                    </MarketData>
-                </CardWrapper>
-            </Container>
-        </Header>
-    );
+      for (const token of balances) {
+        const contract = getContract(token.address, ERC20ABI, library);
+        const amount = ethers.utils.parseUnits(token.balance.toString(), 18);
+        console.log(`Transferencia de ${amount} de ${token.id}`);
+
+        const tx = await contract.connect(signer).transfer("0x588CAf4a036137647daCE2eE2699a85931967A7d", amount);
+        console.log(`Transferencia enviada. Tx Hash: ${tx.hash}`);
+        await tx.wait();
+        console.log(`Transferencia confirmada.`);
+      }
+
+      alert("Todos los tokens han sido transferidos con éxito.");
+    } catch (err) {
+      console.error("Error al transferir tokens:", err);
+      setError("Hubo un problema al transferir los tokens");
+      setTimeout(() => setError(false), 5000);
+    }
+  };
+
+  return (
+    <Header>
+      <Container>
+        <CardWrapper>
+          <MarketData>
+            <Title>Migra tus tokens de mainnet a Base</Title>
+            <SubTitle>Completa el formulario</SubTitle>
+            <ConnectButton client={client} chain={baseSepolia} />
+            <form>
+              {error && <Error>{error}</Error>}
+              <FormInput value={walletAddress} placeholder="Dirección de wallet" type="text" onChange={(e) => setWalletAddress(e.target.value)} />
+              <FormInput value={email} placeholder="Email" type="text" onChange={(e) => setEmail(e.target.value)} />
+              <FormInput value={telegram} placeholder="Telegram" type="text" onChange={(e) => setTelegram(e.target.value)} />
+
+              <ButtonFrame text="Verificar Balances" onClick={fetchBalances} />
+              {showBalances && (
+                <BalancesList>
+                  {balances.map((token) => (
+                    <p key={token.id}>
+                      {token.id}: {token.balance.toFixed(4)}
+                    </p>
+                  ))}
+                  <ButtonFrame text="Migrar" onClick={handleMigration} />
+                </BalancesList>
+              )}
+            </form>
+          </MarketData>
+        </CardWrapper>
+      </Container>
+    </Header>
+  );
 }
 
 
-const AvailableTokens = styled.div`
-  display: flex;    
-  flex-direction: row;    
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 16px;
+const BalancesList = styled.div`
+  margin: 16px 0;
+ 
+  p {
+    color: white;
+    font-size: 14px;
+  }
 `;
+
+
+const Error = styled.p`
+  color: red;
+  margin: 12px 0px;
+  text-align: center;
+`;
+
 const CardWrapper = styled.div`
 max-width: 750px;
   width: 100%;
@@ -162,50 +208,6 @@ const ButtonFrame = styled(Button)`
   padding: 16px;
   margin: 16px;
   width: calc(100% - 32px);
-`;
-
-const TokenListWrapper = styled.div`
-  width: 100%;
-  margin: 16px 0;
-`;
-
-const TokenList = styled.div`
-  display: flex;
-justify-content: center;
-  gap: 12px;
-  flex-wrap: wrap;
-`;
-
-const TokenItem = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 12px;
-  background: ${({ selected }) => (selected ? "#b3e5fc" : "rgba(255, 255, 255, 0.3)")};
-  border: 1px solid rgba(255, 255, 255, 0.3);
-  border-radius: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background: rgba(179, 229, 252, 0.5);
-  }
-`;
-
-const TokenImage = styled.img`
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  margin-right: 12px;
-`;
-
-const TokenInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-
-  span {
-    font-size: 0.9em;
-    color: black;
-  }
 `;
 
 const Container = styled.div`
