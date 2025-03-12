0511a652f73bf2e1fb1ad2100a69b017f4b63133
Juan Arguello <arguellojuan08@gmail.com>
2025-03-12 19:34:07 -0300

    ricardo-santos

--- src/components/ProductWinerySelector/ProductWinerySelector.jsx
+++ src/components/ProductWinerySelector/ProductWinerySelector.jsx
@@ -51,6 +51,19 @@ const ProductWinerySelector = ({ winery }) => {
 								/>
 							</div>
 						)}
+
+						{winery.name === "Ricardo Santos" && (
+							<div className={`${styles["card-img-overlay"]} card-img-overlay`}>
+								<Countdown
+									year={2025}
+									month={5}
+									day={6}
+									hours={12}
+									minutes={0}
+									seconds={0}
+								/>
+							</div>
+						)}
 					</div>
 					<h5 className={styles["card-title"]} style={{ color: "#141414" }}>
 						{winery.name}
