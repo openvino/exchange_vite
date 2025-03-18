f7b5877d2a21dfb36f88263a716742973c1307a7
Pablo Levy <pabloelevy@gmail.com>
2025-03-18 20:46:50 -0300

    static-images

--- src/components/Selector/ProductSelector/ProductSelector.jsx
+++ src/components/Selector/ProductSelector/ProductSelector.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import styles from "./ProductSelector.module.css";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAppContext } from "../../../context";
+import { getTokenImageUrl } from "../../../utils/getStaticImages";
 const ProductSelector = ({ product }) => {
 	const { wineryId } = useParams();
 	const [state, setState] = useAppContext();
@@ -10,7 +11,11 @@ const ProductSelector = ({ product }) => {
 	const handleProductClick = () => {
 		navigate(`/${wineryId}/${product.id}`);
 	};
-
+	const { bottleUrl, imageUrl, tokenUrl } = getTokenImageUrl(
+		product.id?.toLowerCase(),
+		wineryId?.toLocaleLowerCase()
+	);
+	console.log(bottleUrl, imageUrl, tokenUrl);
 	return (
 		<div
 			className="col-6 col-sm-6 col-md-4 col-lg-3"
@@ -20,7 +25,7 @@ const ProductSelector = ({ product }) => {
 				<img
 					className={styles["product-selector-img"]}
 					alt={product.id}
-					src={product.image}
+					src={imageUrl}
 				/>
 			</div>
 		</div>
